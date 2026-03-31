a37c2a719a31f9b840210a4c999bab9a6f01feb1
chewoli <20241301112@ogr.msgsu.edu.tr>
2026-03-31 16:03:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let deneme = 20;
+
+
+let x;
+
+let my_bad;
+
+x = 30;
+
+ console.log(deneme);
+
+ console.log(x);
+
+ console.log(my_bad);
+
+ console.log(x);
+
+ const coding = 80
+ console.log(coding);
+
+ let greetings = "hello"
+
+ let isim = 'Alice'
+
+ let space = " "
+
+ 
+ let sev = "87"
+
+ 
+ console.log(deneme + my_bad)
+
+ let total = (deneme + my_bad)
+ 
+ console.log(total)
+
+ let h = greetings + isim
+
+ console.log(h)
+ 
+ 
+
+ 
+ 
+ 
+ 
+
+
+
